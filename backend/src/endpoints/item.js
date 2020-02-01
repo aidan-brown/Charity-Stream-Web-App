797399e4098726337ff20228b14be44632db8ec7
River Marks <rmarks6767@gmail.com>
2020-01-31 19:23:44 -0500

    Created the item querying functionality and started the item creation functionality

--- backend/src/endpoints/item.js
+++ backend/src/endpoints/item.js
@@ -1,9 +1,54 @@
 import { SqlConnect } from "../sqlConnection";
 
-export function GetItemById(id){
+export function GetItemByWhere(where){
+    const connection = SqlConnect();
+
+    if (where){
+        connection.query( `SELECT * 
+                       FROM items 
+                       WHERE ${where}`, 
+                       (error, result) => {
+            if (error){
+                connection.end();
+                
+                return "Could not find the given item!";
+            }
+            else {
+                connection.end();
+
+                return result.map(value => {
+                    var data = {};
+                    for(key in value) data[key] = value[key];
+                    return data;
+                });
+            }
+        });
+    }
+    else {
+        connection.end();
+        return "No where clause (or id) specified!";
+    }
     
 }
 
-export function GetItemsWhere(where){
+export function CreateItem(item){
+    const connection = SqlConnect();
 
+    if (item){
+        connection.query(`
+            INSERT INTO items 
+            (${Object.keys(item).join()}) 
+            VALUES ("${Object.values(item).join(",")}")`, 
+            (error, result) => {
+                if (error){
+                    return "There was an error inserting this item!";
+                }
+                else {
+                    return "Success!";
+                }
+        });
+    }
+    else {
+        "No item was povided!";
+    }
 }
--- backend/src/server.js
+++ backend/src/server.js
@@ -1,9 +1,11 @@
 import express from "express";
 import bodyParser from "body-parser"; // This is for being able to do posts
-import { GetItemById, GetItemsWhere } from "./endpoints/item"; 
+import { GetItemsWhere, CreateItem } from "./endpoints/item"; 
 
 // Use express
 const app = express();
+const port = 8888;
+
 app.use(bodyParser.urlencoded({ extended: false }))
 
 // Getting a given item based off of an id
@@ -12,16 +14,28 @@ app.get("/item", (req,res) => {
     const where = req.body.where;
 
     if (id && where){
+        res.sendStatus(400);
         return "Cannot specify both!";
     }
     else if (id){
-        return GetItemById(id);
+        return GetItemByWhere(`id = '${id}'`);
     }
     else if (where){
         return GetItemsWhere(where);
     }
     else {
+        res.sendStatus(400);
         return "Must specify an id or a where clause!";
+    }
+});
+
+app.get("/create/item", (req, res) => {
+    if(req.body.item){
+        return CreateItem(req.body.item);
+    }
+    else {
+        res.sendStatus(400);
+        return "No item was provided!";
     }
 });
 
