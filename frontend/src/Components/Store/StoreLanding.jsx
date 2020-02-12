ed62079dd563cfe09ea415fabf27486ae95b1941
Corey Rigney <coreyrigney@gmail.com>
2020-02-11 23:00:01 -0500

    Added Weapons to the loop

--- frontend/src/Components/Store/StoreLanding.jsx
+++ frontend/src/Components/Store/StoreLanding.jsx
@@ -4,13 +4,87 @@ import All from './StoreTabs/All';
 class StoreLanding extends Component{
     constructor(props){
         super(props);
-        this.items = [];
+        this.items = [{
+            "id": 1,
+            "name": "Iron Ingot",
+            "description": "An ingot full of potential. Can be used to craft iron armor, weapons, and tools.",
+            "price": 0.25,
+            "type": {
+                "name": "material"
+            }
+        },
+        {
+            "id": 2,
+            "name": "Gold Ingot",
+            "description": "An ingot full of potential. Can be used to craft gold armor, weapons, and tools.",
+            "price": 0.20,
+            "type": {
+                "name": "material"
+            }
+        },
+        {
+            "id": 3,
+            "name": "Diamond",
+            "description": "A shiny gem. Can be used to craft powerful diamond armor, weapons, and tools.",
+            "price": 1.00,
+            "type": {
+                "name": "material"
+            }
+        }
+        ];
+        this.weapons = [
+            {
+                "id": 17,
+                "name": "Stone Sword",
+                "description": "The basic weapon for combat. Help your favorite player triump in battle!",
+                "price": 0.30,
+                "type": {
+                    "name": "weapon",
+                    "damage": 5,
+                    "durability": 131
+                }
+            },
+            {
+                "id": 18,
+                "name": "Iron Sword",
+                "description": "An upgraded sword for combat. Help your favorite player triump in battle!",
+                "price": 0.50,
+                "type": {
+                    "name": "weapon",
+                    "damage": 6,
+                    "durability": 250
+                }
+            },
+            {
+                "id": 19,
+                "name": "Diamond Sword",
+                "description": "The ultimate sword for combat. Help your favorite player triump in battle with style!",
+                "price": 2.00,
+                "type": {
+                    "name": "weapon",
+                    "damage": 7,
+                    "durability": 1561
+                }
+            },
+            {
+                "id": 20,
+                "name": "Bow",
+                "description": "Minecraft's signature ranged weapon. Can take out foes at a distance!",
+                "price": 1.50,
+                "type": {
+                    "name": "weapon",
+                    "damage": 6,
+                    "durability": 384
+                }
+            }
+        ]
     }
+    
 
     render(){
         return(
             <div className="StoreLanding">
-                <All items={this.items}/>
+                <All items={this.items} weapons={this.weapons}/>
             </div>
         )
     }
--- frontend/src/Components/Store/StoreTabs/All.jsx
+++ frontend/src/Components/Store/StoreTabs/All.jsx
@@ -4,7 +4,8 @@ class All extends Component{
     constructor(props){
         super(props);
         this.state = {
-            items: this.props.items
+            items: this.props.items,
+            weapons: this.props.weapons
         }
 
         this.filter = this.filter.bind(this);
@@ -14,7 +15,8 @@ class All extends Component{
         this.setState({items: []});
     }
     render(){
-        const Items = ({items}) => (
+        const Items = ({items}) => 
+            (
             <>
               {items.map(item => (
                 <div className="item" key={item.id}>
@@ -30,10 +32,31 @@ class All extends Component{
               ))}
             </>
         ); 
-        
+
+
+               
+
+        const Weapons = ({items}) =>
+        ( <>
+            {items.map(item => (
+              <div className="item" key={item.id}>
+                  <div className="itemImgBox">
+                      <img className="itemImg" src="imgPath" alt={item.name}></img>
+                      <h4 className="itemPrice">${item.price}</h4>
+                  </div>
+                  <div className="itemDescBox">
+                      <p className="itemDesc">{item.description}</p>
+                      <p className="moreInfo">{item.type.damage} {item.type.durability}</p>
+                      <button className="shopButton"></button>
+                  </div>
+              </div>
+            ))}
+        </>);
+
         return(
             <div className="AllItems">
                 <Items items={this.state.items} />
+                <Weapons items={this.state.weapons} />
                 <button onClick={this.filter}></button>
             </div>
         );
