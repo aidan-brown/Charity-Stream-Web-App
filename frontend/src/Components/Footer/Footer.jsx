1d0180c4286498a6c32c794205ee0e07d0f98309
Rhys Stever <rhys@stever.com>
2020-02-12 20:10:42 -0800

    Played around with links to github and csh site

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -6,17 +6,23 @@ import "./Footer.css";
 class Footer extends Component{
     render(){
         return(
-            <div className="Footer bg-csh-primary font-csh-dark">
+            <div className="Footer bg-csh-primary">
                 <div className="authors">
-                    <p className="authors-names">Site created by Aidan Brown, River Marks, Corey Rigney, and Rhys Stever</p>
+                    <p className="authors-names font-csh-dark">Site created by Aidan Brown, River Marks, Corey Rigney, and Rhys Stever</p>
                 </div>
-                <div className="github">
-                    <a href="https://github.com/aidan-brown/Charity-Stream-Web-App">Check out this site on Github!</a>
-                    <img id='github-logo' src={GithubLogo} alt='Github logo'/>
-                </div>
-                <div className="pubsite">
-                    <a href="https://csh.rit.edu">Check out CSH!</a>
-                    <img id='csh-logo' src={CSHLogo} alt='CSH logo'/>
+                <div className="links">
+                    <div className="github">
+                        <a href="https://github.com/aidan-brown/Charity-Stream-Web-App">
+                            Check out this site on Github!
+                            <img id='github-logo' src={GithubLogo} alt='Github logo'/>
+                        </a>
+                    </div>
+                    <div className="pubsite">
+                        <a href="https://csh.rit.edu">
+                            Check out CSH!
+                            <img id='csh-logo' src={CSHLogo} alt='CSH logo'/>
+                        </a>
+                    </div>
                 </div>
             </div>
         )
