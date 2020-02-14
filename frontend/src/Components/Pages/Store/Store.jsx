58bba0dbea4e33f1d0422bcaf71706b33e27abff
Aidan <Abrownie99600@gmail.com>
2020-02-13 20:44:43 -0500

    Jesus Corey! Don't push broken code!

---
+++ frontend/src/Components/Pages/Store/Store.jsx
@@ -0,0 +1,14 @@
+import React, { Component } from 'react';
+import All from './StoreTabs/All'
+
+class Store extends Component{
+    render(){
+        return(
+            <div className='Store'>
+                
+            </div>
+        );
+    }
+}
+
+export default Store;
---
+++ frontend/src/Components/Pages/Store/StoreTabs/All.jsx
@@ -0,0 +1,22 @@
+import React, { Component } from 'react';
+
+// class All extends Component{
+//     render(){
+//         handleEvents = (array) => {
+//             if(array.length > 0){
+//              let tempArray = [];
+//              array.forEach(function(each){
+//                tempArray.push(<h1>hello {each.name}</h1>);
+//             })
+//              return tempArray;
+//            }
+//             return(
+//                 <div className="All">
+                
+//                 </div>
+//             );
+//         }
+//     }
+// }
+
+// export default All;
