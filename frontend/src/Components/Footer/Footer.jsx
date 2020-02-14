cd4bfc20167d0316ec4cf8b1a73f21ce748b3815
Aidan <Abrownie99600@gmail.com>
2020-02-13 20:34:43 -0500

    Made footer responsive

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -1,23 +1,23 @@
 import React, { Component } from 'react';
-import CSHLogo from '../../images/csh_logo_round.svg';
+import CSHLogo from '../../images/csh.svg';
 import GithubLogo from '../../images/github.svg';
 import "./Footer.css";
 
 class Footer extends Component{
     render(){
         return(
-            <div className="Footer bg-csh-primary">
+            <div className="Footer bg-csh-primary-gradient">
                 <div className="authors">
                     <p className="authors-names font-csh-dark">Site created by Aidan Brown, River Marks, Corey Rigney, and Rhys Stever</p>
                 </div>
                 <div className="links">
                     <a href="https://github.com/aidan-brown/Charity-Stream-Web-App" className="github">
-                        <p className="githubLink font-csh-dark">Check out this site on Github!
-                        <img id='github-logo' src={GithubLogo} alt='Github logo'/></p> 
+                        <p className="githubLink font-csh-dark">Check out this site on Github!</p>
+                        <img id='github-logo' src={GithubLogo} alt='Github logo'/>
                     </a>
                     <a href="https://csh.rit.edu" className="pubsite">
-                        <p className="pubsiteLink font-csh-dark">Check out CSH!
-                        <img id='csh-logo' src={CSHLogo} alt='CSH logo'/></p>
+                        <p className="pubsiteLink font-csh-dark">Check out CSH!</p>
+                        <img id='csh-logo' src={CSHLogo} alt='CSH logo'/>
                     </a>
                 </div>
             </div>
