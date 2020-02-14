0cc3ac47cd78f6e2f6e2bc18cfe232c2627f6460
Rhys Stever <rhys@stever.com>
2020-02-13 19:34:29 -0800

    Fixed hovering over links to github and pubsite

--- frontend/src/Components/Footer/Footer.jsx
+++ frontend/src/Components/Footer/Footer.jsx
@@ -11,18 +11,14 @@ class Footer extends Component{
                     <p className="authors-names font-csh-dark">Site created by Aidan Brown, River Marks, Corey Rigney, and Rhys Stever</p>
                 </div>
                 <div className="links">
-                    <div className="github">
-                        <a href="https://github.com/aidan-brown/Charity-Stream-Web-App">
-                            Check out this site on Github!
-                            <img id='github-logo' src={GithubLogo} alt='Github logo'/>
-                        </a>
-                    </div>
-                    <div className="pubsite">
-                        <a href="https://csh.rit.edu">
-                            Check out CSH!
-                            <img id='csh-logo' src={CSHLogo} alt='CSH logo'/>
-                        </a>
-                    </div>
+                    <a href="https://github.com/aidan-brown/Charity-Stream-Web-App" className="github">
+                        <p className="githubLink font-csh-dark">Check out this site on Github!
+                        <img id='github-logo' src={GithubLogo} alt='Github logo'/></p> 
+                    </a>
+                    <a href="https://csh.rit.edu" className="pubsite">
+                        <p className="pubsiteLink font-csh-dark">Check out CSH!
+                        <img id='csh-logo' src={CSHLogo} alt='CSH logo'/></p>
+                    </a>
                 </div>
             </div>
         )
