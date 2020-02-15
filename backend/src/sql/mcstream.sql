5201365409a22347b525c5bf72d3027ae35f22cc
River Marks <rmarks6767@gmail.com>
2020-02-14 23:46:25 -0500

    Changed the sql for the team to be an enum

--- backend/src/sql/mcstream.sql
+++ backend/src/sql/mcstream.sql
@@ -1,7 +1,7 @@
 CREATE TABLE IF NOT EXISTS players(
     username varchar(255) NOT NULL,
     name varchar(255) NOT NULL,
-    team varchar(255) NOT NULL,
+    type ENUM('csh', 'blackbaud', 'hogs', 'ehouse', 'sse', 'arthouse', 'rit'),
     PRIMARY KEY (username)
 );
 
