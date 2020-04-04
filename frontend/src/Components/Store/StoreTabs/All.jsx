3a22ecc91ba55746483720e378cebf968c0e40cb
Corey Rigney <coreyrigney@gmail.com>
2020-04-04 15:15:07 -0500

    Reduced old broken code

--- frontend/src/Components/Store/StoreTabs/All.jsx
+++ frontend/src/Components/Store/StoreTabs/All.jsx
@@ -1,27 +1,17 @@
 import React, { Component } from 'react';
+import { connect } from 'react-redux'
 
 class All extends Component{
     constructor(props){
         super(props);
-        this.state = {
-            items: this.props.items,
-            weapons: this.props.weapons
-        }
-
-        this.filter = this.filter.bind(this);
     }
 
-    filter(){
-        this.setState({items: []});
-    }
     render(){
-        const Items = ({items}) => 
-            (
-            <>
-              {items.map(item => (
+        let itemList = this.props.items.map(item=>{
+            return(
                 <div className="item" key={item.id}>
                     <div className="itemImgBox">
-                        <img className="itemImg" src="imgPath" alt={item.name}></img>
+                        <img className="itemImg" src={item.img} alt={item.name}></img>
                         <h4 className="itemPrice">${item.price}</h4>
                     </div>
                     <div className="itemDescBox">
@@ -29,42 +19,23 @@ class All extends Component{
                         <button className="shopButton"></button>
                     </div>
                 </div>
-              ))}
-            </>
-        ); 
-
-
-               
-
-        const Weapons = ({items}) =>
-        ( <>
-            {items.map(item => (
-              <div className="item" key={item.id}>
-                  <div className="itemImgBox">
-                      <img className="itemImg" src="imgPath" alt={item.name}></img>
-                      <h4 className="itemPrice">${item.price}</h4>
-                  </div>
-                  <div className="itemDescBox">
-                      <p className="itemDesc">{item.description}</p>
-                      <p className="moreInfo">{item.type.damage} {item.type.durability}</p>
-                      <button className="shopButton"></button>
-                  </div>
-              </div>
-            ))}
-        </>);
-
+            )
+        })
         return(
-            <div className="AllItems">
-                <Items items={this.state.items} />
-                <Weapons items={this.state.weapons} />
-                <button onClick={this.filter}></button>
+            <div className="container">
+                <h3 className="center">Our items</h3>
+                <div className="box">
+                    {itemList}
+                </div>
             </div>
-        );
-    }
-
-    componentDidMount(){
-
+        )
     }
 }
 
-export default All;
+const mapStateToProps = (state)=>{
+    return {
+        items: state.items
+         }
+    }
+
+export default connect(mapStateToProps)(All)
