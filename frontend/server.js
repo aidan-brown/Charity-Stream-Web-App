a28e0e4913fe969a040aad501b1257cf56f5f75c
River Marks <rmarks6767@gmail.com>
2021-03-03 20:34:38 -0500

    Port

--- frontend/server.js
+++ frontend/server.js
@@ -2,10 +2,12 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
+const PORT = process.env.PORT || 3000;
+
 app.use(express.static(path.join(__dirname, 'build')));
 
 app.get('/', function (_, res) {
   res.sendFile(path.join(__dirname, 'build', 'index.html'));
 });
 
-app.listen(8080, () => console.log('listening on port 8080'));
+app.listen(PORT, () => console.log(`listening on port ${PORT}`));
