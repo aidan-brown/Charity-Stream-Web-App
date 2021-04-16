5c100b8240f93d0962dc7ef204d6e2423be0a72a
Aidan Brown <38188326+aidan-brown@users.noreply.github.com>
2021-04-16 12:40:46 -0400

    Fixed backend connection to frontend and worked more on the cart functionality and style

--- frontend/src/Components/Pages/Store/Cart/Cart.jsx
+++ frontend/src/Components/Pages/Store/Cart/Cart.jsx
@@ -2,20 +2,22 @@ import React, { useState, useEffect } from 'react';
 
 import './Cart.css';
 
-const Cart = ({cartItems, removeItemFromCart, proceedToCheckout}) => {
+const Cart = ({cartItems, removeItemFromCart, proceedToCheckout, showCart}) => {
     
 
     return(
-        <div className="Cart bg-csh-secondary-gradient">
-            {cartItems.map((item, index) => {
-                return <span key={index} className='cart-item'>
-                    <p className='cart-item-name'>{item.name}</p>
-                    <p className='cart-item-price'>${item.price.toFixed(2)}</p>
-                    <p className='cart-item-amount'>{item.amount}</p>
-                    <p className='cart-item-total'>${(item.amount * item.price).toFixed(2)}</p>
-                    <button onClick={() => removeItemFromCart(index)}>Remove</button>
-                </span>
-            })}
+        <div className="Cart bg-csh-secondary-gradient" data-showCart={showCart}>
+            <div className="cart-content">
+                {cartItems.map((item, index) => {
+                    return <span key={index} className='cart-item'>
+                        <p className='cart-item-name'>{item.name}</p>
+                        <p className='cart-item-price'>${item.price.toFixed(2)}</p>
+                        <p className='cart-item-amount'>{item.amount}</p>
+                        <p className='cart-item-total'>${(item.amount * item.price).toFixed(2)}</p>
+                        <button onClick={() => removeItemFromCart(index)}>Remove</button>
+                    </span>
+                })}
+            </div>
             <button onClick={proceedToCheckout}>Proceed To Checkout</button>
         </div>
     )
