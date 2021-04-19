26aa4e88fe68b2fd3d4f37661516c14a7932a262
Corey Rigney <coreyrigney@gmail.com>
2021-04-19 15:07:26 -0400

    Readded statTracking

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -2,17 +2,17 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './Components/App/App';
-// import { datadogRum } from '@datadog/browser-rum';
+import { datadogRum } from '@datadog/browser-rum';
 
-// datadogRum.init({
-//     applicationId: window._env_.DDAPPID,
-//     clientToken: window._env_.DDCLITOKEN,
-//     site: 'datadoghq.com',
-//     service: 'Minecraft-Charity-Stream',
-// //  env: 'production',
-// //  version: '1.0.0',
-//     sampleRate: 100,
-//     trackInteractions: true
-// });
+datadogRum.init({
+    applicationId: window._env_.DDAPPID,
+    clientToken: window._env_.DDCLITOKEN,
+    site: 'datadoghq.com',
+    service: 'Minecraft-Charity-Stream',
+    env: 'production',
+    version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
