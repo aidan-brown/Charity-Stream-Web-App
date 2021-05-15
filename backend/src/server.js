a60cbf26b8d579c1cb063f168776bb23e7fdbd83
River Marks <rmarks6767@gmail.com>
2021-05-15 12:29:15 -0400

    Finished updating the routes to support the new site stuff

--- backend/src/server.js
+++ backend/src/server.js
@@ -2,7 +2,14 @@
 const express = require('express');
 const cors = require('cors');
 const {
-  createItems, createPlayer, getItems, getPlayers,
+  createItems, 
+  createPlayer, 
+  createEffects, 
+  createMobs, 
+  getItems, 
+  getPlayers, 
+  getEffects, 
+  getMobs 
 } = require('./handlers');
 
 const app = express();
@@ -13,7 +20,11 @@ app.use(cors());
 
 app.post('/create/items', createItems);
 app.post('/create/players', createPlayer);
+app.post('/create/effects', createEffects);
+app.post('/create/mobs', createMobs);
 app.get('/items', getItems);
 app.get('/players', getPlayers);
+app.get('/effects', getEffects);
+app.get('/mobs', getMobs);
 
 app.listen(port, console.log(`Listening on port at http://localhost:${port}`));
