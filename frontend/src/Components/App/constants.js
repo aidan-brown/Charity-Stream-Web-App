e818538f92f2782cce8fd565e9db8fd94aee8b2a
Aidan Brown <38188326+aidan-brown@users.noreply.github.com>
2021-05-17 18:29:17 -0400

    AHHHHHHHHH

--- frontend/src/Components/App/constants.js
+++ frontend/src/Components/App/constants.js
@@ -1,2 +1,3 @@
 export const BACKENDURL = 'https://charity-stream-minecraft-charity-stream.apps.okd4.csh.rit.edu';
+export const AWSURL = 'https://ob1wqrrptc.execute-api.us-east-1.amazonaws.com/prod/custom';
 export const JG_FUNDRAISING_ID = 14120472;
