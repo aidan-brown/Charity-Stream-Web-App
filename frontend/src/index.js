9ef553e2d4c9dbaaf8d562320ab9a2ae1bbea1e1
Aidan Brown <38188326+aidan-brown@users.noreply.github.com>
2021-05-17 18:29:56 -0400

    AHHHHHHHHHH

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -3,17 +3,17 @@ import './index.css';
 import App from './Components/App/App';
 import React from 'react';
 import ReactDOM from 'react-dom';
-// import { datadogRum } from '@datadog/browser-rum-recorder';
+import { datadogRum } from '@datadog/browser-rum-recorder';
 
-// datadogRum.init({
-//     applicationId: window._env_.DDAPPID,
-//     clientToken: window._env_.DDCLITOKEN,
-//     site: 'datadoghq.com',
-//     service: 'Minecraft-Charity-Stream',
-//     env: 'production',
-//     version: '1.0.0',
-//     sampleRate: 100,
-//     trackInteractions: true
-// });
+datadogRum.init({
+    applicationId: window._env_.DDAPPID,
+    clientToken: window._env_.DDCLITOKEN,
+    site: 'datadoghq.com',
+    service: 'Minecraft-Charity-Stream',
+    env: 'production',
+    version: '1.0.0',
+    sampleRate: 100,
+    trackInteractions: true
+});
 
 ReactDOM.render(<App />, document.getElementById('root'));
