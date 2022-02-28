ef0c300c06a1f0d92f2d746a4de497a8ad726238
jabbate19 <josephabbateny@gmail.com>
2022-02-28 12:27:04 -0500

    Add RCON Control to Hook

--- backend/src/handlers/hook.js
+++ backend/src/handlers/hook.js
@@ -1,6 +1,9 @@
 const stripe = require('stripe')(process.env.STRIPE_KEY);
+const Rcon = require('modern-rcon');
 
 const endpointSecret = process.env.STRIPE_ENDPOINT_SECRET;
+
+const rcon = new Rcon(process.env.MCSERVER, 25569, process.env.MCSERVERPWRD, 5000);
 
 module.exports = {
   hook: async (request, response) => {
@@ -17,6 +20,20 @@ module.exports = {
 
     switch (event.type) {
       case 'checkout.session.completed': {
+        const session = event.data.object;
+        rcon.connect().then(async () => {
+          for (const key in session.metadata) {
+            if (session.metadata[key]) {
+              await rcon.send(session.metadata[key]);
+            }
+          }
+        }).then(() => {
+          rcon.disconnect();
+          response.status(200).send('OK');
+        }).catch((error) => {
+          console.log(error);
+          response.status(500).send(error);
+        });
         // Then define and call a function to handle the event checkout.session.completed
         break;
       }
