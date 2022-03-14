85af160cc83a973a94000302a9fe32a9a501dbbb
River Marks <rmarks6767@gmail.com>
2022-03-14 11:14:13 -0400

    Update constants.js

--- frontend/src/Components/App/constants.js
+++ frontend/src/Components/App/constants.js
@@ -1,5 +1,5 @@
 export const BACKENDURL = process.env.NODE_ENV === 'production'
-  ? 'https://charity-stream-minecraft-charity-stream.apps.okd4.csh.rit.edu'
+  ? 'https://charitystream-app.cs.house'
   : 'http://localhost:8080';
 export const AWSURL = 'https://ob1wqrrptc.execute-api.us-east-1.amazonaws.com/prod/custom';
 export const JG_FUNDRAISING_ID = 14120472;
