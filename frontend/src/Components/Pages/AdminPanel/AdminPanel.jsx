d633ef9ddc0a8450e82416a00a3d401c85d73a75
River Marks <rmarks6767@gmail.com>
2022-03-25 03:38:19 -0400

    Began work on the admin panel, made login screen

--- frontend/src/Components/Pages/AdminPanel/AdminPanel.jsx
+++ frontend/src/Components/Pages/AdminPanel/AdminPanel.jsx
@@ -1,33 +1,32 @@
 import {
-  Button, Checkbox, Input, Grid, Box,
+  Alert, AlertTitle, Button, TextField,
 } from '@mui/material';
 import React, { useEffect, useState } from 'react';
 import { BACKENDURL } from '../../App/constants';
+import StoreItem from '../Store/StoreContent/StoreItem';
 import './AdminPanel.scss';
 
-const TYPES = ['armor', 'food', 'material', 'misc', 'tool', 'weapon'];
-
 const AdminPanel = () => {
-  const [elements, setElements] = useState([]);
-  const [toDisable, setToDisable] = useState([]);
-  const [authHeader, setAuthHeader] = useState();
+  const [loggedIn, setLoggedIn] = useState(false);
   const [username, setUsername] = useState('');
   const [password, setPassword] = useState('');
-  const [loggedIn, setLoggedIn] = useState(false);
+  const [authHeader, setAuthHeader] = useState();
   const [error, setError] = useState();
+  const [toDisable, setToDisable] = useState([]);
+  const [items, setItems] = useState([]);
 
   useEffect(() => {
     const header = localStorage.getItem('mcs-authHeader');
     if (header) {
       setAuthHeader(header);
-      setLoggedIn(true);
+      // setLoggedIn(true);
     }
 
     const getElements = () => {
       fetch(`${BACKENDURL}/minecraft/all`)
         .then((res) => res.json())
         .then((res) => {
-          setElements(res);
+          setItems(res);
         }).catch(() => {
           setError({
             message: 'Could not get items from backend',
@@ -45,10 +44,18 @@ const AdminPanel = () => {
         Authorization,
       },
     })
-      .then(() => {
-        setLoggedIn(true);
-        setAuthHeader(Authorization);
-        localStorage.setItem('mcs-authHeader', Authorization);
+      .then(({ status }) => {
+        console.log(status);
+        if (status === 200) {
+          setLoggedIn(true);
+          setAuthHeader(Authorization);
+          localStorage.setItem('mcs-authHeader', Authorization);
+        } else {
+          console.log('Hello there');
+          setError({
+            message: 'Incorrect Username or Password',
+          });
+        }
       });
   };
 
@@ -70,119 +77,44 @@ const AdminPanel = () => {
   };
 
   return (
-    <Box sx={{ flexGrow: 1 }} className="admin-panel">
+    <div className="admin-panel">
       {!loggedIn && (
         <div className="login">
-          <h1>Login</h1>
-          <div className="login-inputs">
-            <Input
-              className="login-input"
-              required
-              placeholder="Username"
+          {error && (
+          <Alert variant="filled" severity="error">
+            <AlertTitle>Error</AlertTitle>
+            {error.message}
+          </Alert>
+          )}
+          <div className="inputs">
+            <TextField
+              label="Username"
+              variant="filled"
               type="username"
-              onChange={(e) => {
-                setUsername(e.target.value);
-              }}
-            />
-            <Input
-              className="login-input"
               required
-              placeholder="Password"
-              type="password"
-              autoComplete="password"
-              onChange={(e) => {
-                setPassword(e.target.value);
-              }}
+              value={username}
+              onChange={(e) => setUsername(e.target.value)}
             />
+            <TextField
+              label="Password"
+              variant="filled"
+              type="password"
+              required
+              value={password}
+              onChange={(e) => setPassword(e.target.value)}
+            />
+            <Button className="signin" type="submit" variant="contained" onClick={login}>
+              SIGN IN
+            </Button>
           </div>
-          <Button
-            color="primary"
-            variant="contained"
-            disabled={username.length === 0 || password.length === 0}
-            onClick={login}
-          >
-            Login
-          </Button>
         </div>
       )}
       {loggedIn && (
         <>
-          <div>
-            {TYPES.map((t) => (
-              <div key={t}>
-                <h1>{`${t[0].toUpperCase()}${t.slice(1, t.length)}`}</h1>
-                {elements.filter((el) => el.type === t).map((element) => {
-                  const {
-                    id, displayName, disabled, type,
-                  } = element;
-
-                  return (
-                    <div key={`${id}-${type}`}>
-                      <div className="element" key={id}>
-                        <img
-                          className="image"
-                          src={`${BACKENDURL}/assets/${
-                            type === 'mob' || type === 'effect'
-                              ? `${type}s` : 'items'}/${id}.png`}
-                          alt={`${id} disable`}
-                        />
-                        <p>{displayName}</p>
-                        <Checkbox
-                          checked={disabled}
-                          onClick={(e) => {
-                            setElements(elements.map((el) => {
-                              const { checked } = e.target;
-
-                              if (el.id === id) {
-                                if (toDisable.includes(id)) {
-                                  setToDisable([...toDisable.filter((item) => item.id !== id)]);
-                                } else {
-                                  setToDisable([...toDisable, {
-                                    id,
-                                    disabled: checked,
-                                  }]);
-                                }
-
-                                return {
-                                  ...el,
-                                  disabled: checked,
-                                };
-                              }
-                              return el;
-                            }));
-                          }}
-                        />
-                      </div>
-                    </div>
-                  );
-                })}
-              </div>
-            ))}
-          </div>
-          <div className="button-group">
-            <Button
-              className="disabled-button-left"
-              color="primary"
-              variant="contained"
-              disabled={toDisable.length === 0}
-              onClick={() => {
-                setToDisable([]);
-              }}
-            >
-              Clear Current Selection
-            </Button>
-            <Button
-              color="primary"
-              variant="contained"
-              disabled={toDisable.length === 0}
-              onClick={disableItems}
-            >
-              Disable Current Selection
-            </Button>
-          </div>
+          {items.map((item) => <StoreItem item={item} addItemToCart={() => {}} />)}
         </>
       )}
-    </Box>
+    </div>
   );
 };
 
