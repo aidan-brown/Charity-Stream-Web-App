e4d43c968076a945273403696ab728ada6a06f30
River Marks <rmarks6767@gmail.com>
2022-03-28 23:10:49 -0400

    Updates to DEPLOYMENT_ENV

--- backend/src/utils/getUrl.js
+++ backend/src/utils/getUrl.js
@@ -1,7 +1,7 @@
 const { PROD_URL, DEV_URL, LOCAL_URL } = require('../constants');
 
 module.exports = () => {
-  switch (process.NODE_ENV) {
+  switch (process.env.DEPLOYMENT_ENV) {
     case 'production': return PROD_URL;
     case 'develop': return DEV_URL;
     case 'local': return LOCAL_URL;
