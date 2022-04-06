24aba8bca21ed6b1cfd52ec69817522216084c98
Aidan Brown <Abrownie99600@gmail.com>
2022-04-06 01:13:01 -0400

    Fixed merge errors; Resolved @rmarks6767 issue

--- frontend/src/Components/Pages/Store/StoreContent/StoreItem.jsx
+++ frontend/src/Components/Pages/Store/StoreContent/StoreItem.jsx
@@ -7,45 +7,29 @@ import { getUrl, ItemSymbols } from '../../../../Utils';
 
 const StoreItem = ({
   item, addItemToCart, isStore, className,
-}) => {
-  const ItemSymbols = (type, rating = 1) => {
-    const symbols = [];
-    let count = rating;
-    for (let i = 0; i < Math.floor(count / 2); i += 1) {
-      symbols.push(<img key={i + type} src={itemSymbols[type]} alt={type} />);
-    }
-    count %= 2;
-    if (count > 0) symbols.push(<img key={`half-${type}`} src={itemSymbols[`${type}Half`]} alt={type} />);
-    return (
-      <>
-        {symbols}
-      </>
-    );
-  };
-
-  return (
-    <span tabIndex={0} role="button" className={`store-item bg-csh-tertiary ${className || ''}`} onClick={addItemToCart} onKeyDown={addItemToCart} data-disabled={item.disabled}>
-      <div className="store-item-header bg-csh-primary-gradient">
-        <LazyLoadImage className="store-item-image" src={`${getUrl()}/images/items/${item.id}-full.webp`} alt={item.displayName} effect="blur" />
-        <LazyLoadImage className="store-item-icon" src={`${getUrl()}/images/items/${item.id}.webp`} alt={item.displayName} effect="blur" />
-        <p className="store-item-displayName">{item.displayName}</p>
-        <p className="store-item-price">
-          $
-          {item.price.toFixed(2)}
-        </p>
-      </div>
-      <div className="store-item-text">
-        <span className="store-item-description">
-          <h3>Description</h3>
-          <p>{item.description}</p>
-          {item.effects && (
+}) => (
+  <span tabIndex={0} role="button" className={`store-item bg-csh-tertiary ${className || ''}`} onClick={addItemToCart} onKeyDown={addItemToCart} data-disabled={item.disabled}>
+    <div className="store-item-header bg-csh-primary-gradient">
+      <LazyLoadImage className="store-item-image" src={`${getUrl()}/images/items/${item.id}-full.webp`} alt={item.displayName} effect="blur" />
+      <LazyLoadImage className="store-item-icon" src={`${getUrl()}/images/items/${item.id}.webp`} alt={item.displayName} effect="blur" />
+      <p className="store-item-displayName">{item.displayName}</p>
+      <p className="store-item-price">
+        $
+        {item.price.toFixed(2)}
+      </p>
+    </div>
+    <div className="store-item-text">
+      <span className="store-item-description">
+        <h3>Description</h3>
+        <p>{item.description}</p>
+        {item.effects && (
           <>
             <h3>Special Effects</h3>
             <p>{item.effects}</p>
           </>
-          )}
-        </span>
-        {(item.durability || item.hungerFill)
+        )}
+      </span>
+      {(item.durability || item.hungerFill)
         && (
         <div className="store-item-stats">
           {item.speed && (
@@ -53,8 +37,8 @@ const StoreItem = ({
             <h3>Speed Rating</h3>
             <p>{item.speed}</p>
           </span>
-        )}
-        {item.damage && (
+          )}
+          {item.damage && (
           <span key={1}>
             <h3>Damage Rating</h3>
             <p>
@@ -65,8 +49,8 @@ const StoreItem = ({
               )
             </p>
           </span>
-        )}
-        {item.protection && (
+          )}
+          {item.protection && (
           <span key={2}>
             <h3>Armor Rating</h3>
             <p>
@@ -77,8 +61,8 @@ const StoreItem = ({
               )
             </p>
           </span>
-        )}
-        {item.hungerFill && (
+          )}
+          {item.hungerFill && (
           <span key={4}>
             <h3>Food Rating</h3>
             <p>
@@ -98,11 +82,10 @@ const StoreItem = ({
           )}
         </div>
         )}
-      </div>
-      {isStore && <Icon path={mdiCartPlus} className="add-cart" />}
-    </span>
-  );
-};
+    </div>
+    {isStore && <Icon path={mdiCartPlus} className="add-cart" />}
+  </span>
+);
 
 StoreItem.propTypes = {
   item: PropTypes.shape({
