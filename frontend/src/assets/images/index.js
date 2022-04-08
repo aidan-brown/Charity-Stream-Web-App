6eab1e8669168a840208ec983f4d715fcff213b6
Aidan Brown <38188326+aidan-brown@users.noreply.github.com>
2022-04-08 00:50:42 -0400

    Added more streamer branding

--- frontend/src/assets/images/index.js
+++ frontend/src/assets/images/index.js
@@ -1,3 +1,4 @@
+/* eslint-disable camelcase */
 import bohchoi from './bohchoi.webp';
 import cahriid from './cahriid.webp';
 import clipsothealien from './clipsothealien.webp';
@@ -6,6 +7,9 @@ import dawnshadowx3 from './dawnshadowx3.webp';
 import laqqy from './laqqy.webp';
 import lumiinara from './lumiinara.webp';
 import mysticat from './mysticat.webp';
+import roxkstar74 from './roxkstar74.webp';
+import samanthiiana from './samanthiiana.webp';
+import skeleton_weeb from './skeleton_weeb.webp';
 import pepband from './pepband.webp';
 
 import armor from './armor.webp';
@@ -16,7 +20,18 @@ import hunger from './hunger.webp';
 import hungerHalf from './half-hunger.webp';
 
 export {
-  bohchoi, cahriid, clipsothealien, dantayy5050, dawnshadowx3, laqqy, lumiinara, mysticat, pepband,
+  bohchoi,
+  cahriid,
+  clipsothealien,
+  dantayy5050,
+  dawnshadowx3,
+  laqqy,
+  lumiinara,
+  mysticat,
+  roxkstar74,
+  samanthiiana,
+  skeleton_weeb,
+  pepband,
 };
 
 export const itemSymbols = {
