d13a596235b72d6646e71f4e8a8cad0457406f6f
Mark Anderson <andersonmark@microsoft.com>
2023-09-11 14:36:42 -0700

    Removing countdown stuff and changing colors (will need to solidify later)

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -5,21 +5,9 @@ import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
-import { datadogRum } from '@datadog/browser-rum-recorder';
 import App from './Components/App/App';
 import './index.css';
 
-/* eslint-disable no-underscore-dangle */
-datadogRum.init({
-  applicationId: window._env_.DDAPPID,
-  clientToken: window._env_.DDCLITOKEN,
-  site: 'datadoghq.com',
-  service: 'Minecraft-Charity-Stream',
-  env: 'production',
-  version: '1.0.0',
-  sampleRate: 100,
-  trackInteractions: true,
-});
 /* eslint-enable no-underscore-dangle */
 
 // Create a client
